51f6d53528e850021fd7b465795718cfd12fab94
Santiago <fsgreco@fastmail.com>
2025-08-24 19:12:30 +0200

    Refactor decoupling rendering logic  and display bunny sign for functionality

--- bunnysign/bunnysign.go
+++ bunnysign/bunnysign.go
@@ -7,29 +7,50 @@ import (
 	"time"
 )
 
+func CreatePlayer(FrameGenerator func(content string) string) func(content string) {
+	previousFrameLines := 0
+
+	return func(content string) {
+		if previousFrameLines > 0 {
+			clearLines(previousFrameLines)
+		}
+		// print the frame
+		frame := FrameGenerator(content)
+		fmt.Printf("%s\n", frame)
+
+		previousFrameLines = strings.Count(frame, "\n") + 1
+		// aspetta il tempo necessario (se è ultimo frame aspetta)
+		time.Sleep(time.Duration(rand.Intn(300)+100) * time.Millisecond)
+	}
+}
+
+// Clears the last n lines from the terminal.
+// n is the number of lines to clear.
+// Note: This function assumes the terminal supports ANSI escape codes.
+func clearLines(n int) {
+	if n > 0 {
+		// \033[{n}A moves the cursor up n lines
+		// \033[J clears from the cursor to the end of the screen
+		fmt.Printf("\033[%dA\033[J", n)
+	}
+}
+
 func Display(phrases []string) {
+	render := CreatePlayer(Generate)
 
 	for _, phrase := range phrases {
-		msg := []string{}
+		msg := ""
 		words := strings.Split(phrase, " ")
 
-		// Track the previous frame's line count
-		previousFrameLines := 0
-
 		for i, word := range words {
-			msg = append(msg, word)
-
-			frame := Generate(strings.Join(msg, " "))
 
 			if i > 0 {
-				clearLines(previousFrameLines)
+				msg += " "
 			}
-			fmt.Printf("%s\n", frame)
+			msg += word
 
-			// Save the current frame's line count for the next iteration
-			previousFrameLines = strings.Count(frame, "\n") + 1
+			render(msg)
 
-			time.Sleep(time.Duration(rand.Intn(300)+100) * time.Millisecond)
 			if i == len(words)-1 {
 				time.Sleep(800 * time.Millisecond)
 			}
@@ -50,17 +71,6 @@ func Generate(message string) string {
 	}
 
 	return strings.Join(sign, "\n")
-}
-
-// Clears the last n lines from the terminal.
-// n is the number of lines to clear.
-// Note: This function assumes the terminal supports ANSI escape codes.
-func clearLines(n int) {
-	if n > 0 {
-		// \033[{n}A moves the cursor up n lines
-		// \033[J clears from the cursor to the end of the screen
-		fmt.Printf("\033[%dA\033[J", n)
-	}
 }
 
 func wrapText(phrase string, padding string, maxLength int) string {
