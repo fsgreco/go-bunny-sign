28b767cb1097273001ef78390e7481ea264ecd53
Santiago <fsgreco@fastmail.com>
2025-08-24 19:08:00 +0200

    Cleaning main func - WIP - refactoring

--- bunnysign/bunnysign.go
+++ bunnysign/bunnysign.go
@@ -1,8 +1,41 @@
 package bunnysign
 
 import (
+	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
+
+func Display(phrases []string) {
+
+	for _, phrase := range phrases {
+		msg := []string{}
+		words := strings.Split(phrase, " ")
+
+		// Track the previous frame's line count
+		previousFrameLines := 0
+
+		for i, word := range words {
+			msg = append(msg, word)
+
+			frame := Generate(strings.Join(msg, " "))
+
+			if i > 0 {
+				clearLines(previousFrameLines)
+			}
+			fmt.Printf("%s\n", frame)
+
+			// Save the current frame's line count for the next iteration
+			previousFrameLines = strings.Count(frame, "\n") + 1
+
+			time.Sleep(time.Duration(rand.Intn(300)+100) * time.Millisecond)
+			if i == len(words)-1 {
+				time.Sleep(800 * time.Millisecond)
+			}
+		}
+	}
+}
 
 func Generate(message string) string {
 	padding := strings.Repeat(" ", 4)
@@ -17,6 +50,17 @@ func Generate(message string) string {
 	}
 
 	return strings.Join(sign, "\n")
+}
+
+// Clears the last n lines from the terminal.
+// n is the number of lines to clear.
+// Note: This function assumes the terminal supports ANSI escape codes.
+func clearLines(n int) {
+	if n > 0 {
+		// \033[{n}A moves the cursor up n lines
+		// \033[J clears from the cursor to the end of the screen
+		fmt.Printf("\033[%dA\033[J", n)
+	}
 }
 
 func wrapText(phrase string, padding string, maxLength int) string {
--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/fsgreco/go-bunny-sign/bunnysign"
 )
@@ -24,41 +21,5 @@ func main() {
 	fmt.Println("DEBUG - this line should remain")
 	fmt.Println()
 
-	for _, phrase := range phrases {
-		msg := []string{}
-		words := strings.Split(phrase, " ")
-
-		// Track the previous frame's line count
-		previousFrameLines := 0
-
-		for i, word := range words {
-			msg = append(msg, word)
-
-			frame := bunnysign.Generate(strings.Join(msg, " "))
-
-			if i > 0 {
-				clearLines(previousFrameLines)
-			}
-			fmt.Printf("%s\n", frame)
-
-			// Save the current frame's line count for the next iteration
-			previousFrameLines = strings.Count(frame, "\n") + 1
-
-			time.Sleep(time.Duration(rand.Intn(300)+100) * time.Millisecond)
-			if i == len(words)-1 {
-				time.Sleep(800 * time.Millisecond)
-			}
-		}
-	}
-}
-
-// Clears the last n lines from the terminal.
-// n is the number of lines to clear.
-// Note: This function assumes the terminal supports ANSI escape codes.
-func clearLines(n int) {
-	if n > 0 {
-		// \033[{n}A moves the cursor up n lines
-		// \033[J clears from the cursor to the end of the screen
-		fmt.Printf("\033[%dA\033[J", n)
-	}
+	bunnysign.Display(phrases)
 }
